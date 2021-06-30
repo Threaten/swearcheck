7716350ebb655535880838e1c0206ead720257a2
Threaten <zelgiushoang@gmail.com>
2021-06-30 13:59:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,22 +11,27 @@ client.on("ready", () => {
   console.log("Now Online, ready to filter!");
 });
 
-client.on("message", async (message) => {
-  var role = message.guild.roles.cache.get("850693745133355008");
-  var checkStr = message.content;
-  console.log(message.author.id);
-  var str = checkStr.replace(/[!@#$%^&*=()-_,.?"`:{}|<>+]/g, "");
-  if (message.author.id !== "259733877826912257") {
-    console.log(message.author.id);
-    if (config.FILTER_LIST.some((word) => str.toLowerCase().includes(word))) {
-      if (role) {
-        message.member.roles.add(role);
-        message.reply("alo thằng này chửi thề nè <@692708913396580432>");
-        console.log("1");
-      } else {
-        message.reply("alo thằng này chửi thề nè <@692708913396580432>");
-        console.log("2");
-      }
-    }
-  }
+client.user.setActivity("with depression", {
+  type: "PLAYING",
+  url: "Temporarily offline for enhancements."
 });
+
+//client.on("message", async (message) => {
+  //var role = message.guild.roles.cache.get("850693745133355008");
+  //var checkStr = message.content;
+  //console.log(message.author.id);
+  //var str = checkStr.replace(/[!@#$%^&*=()-_,.?"`:{}|<>+]/g, "");
+  //if (message.author.id !== "259733877826912257") {
+  //  console.log(message.author.id);
+  // if (config.FILTER_LIST.some((word) => str.toLowerCase().includes(word))) {
+  //    if (role) {
+  //      message.member.roles.add(role);
+  //      message.reply("alo thằng này chửi thề nè <@692708913396580432>");
+  //      console.log("1");
+  //    } else {
+  //     message.reply("alo thằng này chửi thề nè <@692708913396580432>");
+  //      console.log("2");
+  //    }
+  //  }
+  // }
+//});
